f81a7fdc6b7a6047e6efc95ee64b883bce82790f
swaraj kaondal <skaonda@ncsu.edu>
2024-09-03 23:27:16 -0400

    Added stack trace and changes suggested by pylint

--- src/rand.py
+++ src/rand.py
@@ -1,10 +1,22 @@
+"""
+This module provides a function to generate a random array using shell commands.
+"""
 import subprocess
 
 
 def random_array(arr):
+    """
+    Fills an array with random integers generated using the `shuf` shell command.
+
+    Parameters:
+    input_arr (list): The list to be populated with random integers.
+
+    Returns:
+    list: The list populated with random integers.
+    """
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
